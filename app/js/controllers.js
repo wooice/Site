0a248faa6344f8f373aec1e0382ce7f413d13f35
Jack Liu <eclipsse@aliyun.com>
2013-07-23 23:21:24 +0800

    Show sound

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,11 +14,37 @@ angular.module('myApp.controllers', []).
 /* Controllers */
 
 function MusicListCtrl($scope, Music) {
-  $scope.musics = Music.query();
+ 
+  $scope.$musics = Music.query();
   $scope.orderProp = 'age';
   
   $scope.musics_top = Music.queryTop();
   $scope.orderProp = 'age';
+  
+  $scope.$on('$viewContentLoaded', function(){
+	  
+	  for (var j=0; j< 6; j++) 
+	  {
+		  var waveData = [];
+			
+	      for(var i =0; i< 1800; i++)
+	      {
+	          waveData[i] = Math.random();
+	      }
+	      var player = new $.player({
+	          id: j,
+	          waveData: waveData,
+	          url: 'resources/sound.mp3',
+	          poster: 'resources/images.jpg',
+	          title: {alias: 'Fushi Mountain', route:'#'},
+	          owner: {alias:'Eason Chen', route:'#'},
+	          duration: 256*1000
+	      });
+	      player.renderSound();  
+	  }
+	  
+	  
+  });
   
 }
 
